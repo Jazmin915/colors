a70de9b36ea8fbe87e19e53e85c56deee9166058
Jazmin Gonzalez <gonzalez.jazmin@student.greenriver.edu>
2023-01-05 16:45:27 -0500

    create form

--- index.php
+++ index.php
@@ -9,7 +9,14 @@
 </head>
 <body>
 
-<h1>Pick a color</h1>
+    <h1>Pick a color</h1>
+    <form method="post" action="confirm.php">
+        <label><input type="radio" value="red" name="color">Red</label><br>
+        <label><input type="radio" value="blue" name="color">Blue</label><br>
+        <label><input type="radio" value="pink" name="color">Pink</label><br>
+        <input type="submit" value="Submit">
+
+    </form>
 
 </body>
 </html>
